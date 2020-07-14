492bb96c02c6fba6a49dbf77838f91b48e883fb1
kasicass <kasicass@gmail.com>
2020-07-14 18:56:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Introduction to 3D Game Programming with DirectX11
+
+ * Effects for Direct3D 11https://github.com/microsoft/FX11
---
+++ src/dx11framework/myxnamath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable: 4838) // warning C4838: conversion from 'unsigned int' to 'INT' requires a narrowing conversion
+
+#include "xnamath.h"
+
+#pragma warning(pop)
